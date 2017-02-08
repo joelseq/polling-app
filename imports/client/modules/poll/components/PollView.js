93bd32a0bb55e8901c8c3717f957cd10e3a23506
Maxwell <mbland@ucsd.edu>
2017-02-07 17:31:57 -0800

    Minor refractoring of code

--- imports/client/modules/poll/components/PollView.js
+++ imports/client/modules/poll/components/PollView.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
+// Grab collection for polls
 import { Polls } from '../../../../api/polling.js';
 
-// App component - represents the whole app
+// Component for a page that displays a poll
 class PollPage extends Component {
   constructor(props) {
     super(props);
   }
 
+  // TODO: Do the job of rendering the poll
   renderPolls() {
 		console.log( this.props.polls[0] );
   }
 
+  // Actual layout
   render() {
     return (
       <div className="container">
@@ -27,13 +30,18 @@ class PollPage extends Component {
   }
 }
 
+// A property containing the polls is required of the data.
 PollPage.propTypes = {
   polls: PropTypes.array.isRequired,
 };
 
+// The real MVP; creates the PollPage container using the routing params
+// (the unique url)
 export default createContainer(({ params }) => {
-  console.log( params.pollId );
-  return {
+  console.log( params.pollId ); // TODO: remove this
+
+  return { // Look in the database for an object with the same id as the 
+           // unique url parameter
     polls: Polls.find({ "_id" : new Mongo.ObjectID(params.pollId) }).fetch(),
   };
 }, PollPage);
