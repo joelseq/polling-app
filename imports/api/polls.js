eafc2c34ca4e7d89f859cc8903197735bebb58b6
Joel Sequeira <joelseq96@gmail.com>
2017-02-19 01:20:58 -0800

    Refactor api/polls.js and add schema with validation for it

--- imports/api/polls.js
+++ imports/api/polls.js
@@ -1,35 +1,44 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 // Collection of all polls
-export const Polls = new Mongo.Collection('polls');
+const Polls = new Mongo.Collection('polls');
 
-// Database call for getting all the polls in the database
-const pollsPublication = function pollsPublication() {
-  return Polls.find({});
-};
+// Schema for Polls
+const PollSchema = new SimpleSchema({
+  name: { type: String },
+  isWeighted: { type: Boolean },
+  options: {
+    type: Object,
+    blackbox: true,
+  },
+  votes: { type: [Object], optional: true },
+  isPrivate: { type: Boolean },
+  password: { type: String, optional: true },
+  createdAt: { type: Date, defaultValue: new Date() },
+});
 
-if (Meteor.isServer) {
-  // Publish all polls in the database, other files may subscribe to this
-  // server call so that they can get the polls
-  Meteor.publish('polls', pollsPublication);
-}
+// Automatically validate the schema for us
+Polls.attachSchema(PollSchema);
 
-// Server methods, mainly database calls
+// Meteor methods are the secure way of making database calls
+// on the client, therefore we define an object of all the
+// possible calls we want to make on the client.
 Meteor.methods({
 
   // Insert a new poll in the database
-  'polls.insert': function dataInsertion(data) {
-    check(data, Object);
+  'polls.insert': function dataInsertion(poll) {
+    // Trivial check to suppress linter warning.
+    // We are already validating schema from the
+    // schema.
+    check(poll, Object);
 
-    // Database calls
-    Polls.insert({
-      data,
-      createdAt: new Date(),
-    });
+    // Database call
+    return Polls.insert(poll);
   },
 
 });
 
-export default Polls; // Required by out lint styler
+export default Polls; // Required by our lint styler
