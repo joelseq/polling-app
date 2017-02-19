755b90a7adaf39be02b3eb2c4c7d9438d6a2a401
Joel Sequeira <joelseq96@gmail.com>
2017-02-19 01:21:17 -0800

    Move polls publication to server/main.js

--- server/main.js
+++ server/main.js
@@ -1,1 +1,10 @@
-import '../imports/api/polls.js';
+import { Meteor } from 'meteor/meteor';
+import Polls from '../imports/api/polls';
+
+// Publications need to be done server-side so it makes more sense to do it in server/main.js
+// rather than imports/api/polls.js
+Meteor.startup(() => {
+  Meteor.publish('polls', function callback() { // eslint-disable-line prefer-arrow-callback
+    return Polls.find({});
+  });
+});
