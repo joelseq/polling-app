0860185dfe066d2e4085157dddd2bce4d95ac661
Joel Sequeira <joelseq96@gmail.com>
2017-02-20 16:41:31 -0800

    Add functionality for weighted Polls

--- imports/client/modules/view-poll/components/ViewPoll.js
+++ imports/client/modules/view-poll/components/ViewPoll.js
@@ -10,9 +10,43 @@ import {
   Checkbox,
   Button,
 } from 'react-bootstrap';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 // Grab collection for polls
 import Polls, { voteHelper } from '../../../../api/polls.js';
+
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const SliderWithTooltip = createSliderWithTooltip(Slider);
+
+// Prop Types for this Component
+const propTypes = {
+  // Poll object in DB from createContainer
+  poll: PropTypes.shape({
+    // Mongo ID for Poll
+    _id: PropTypes.string,
+    // Whether the poll is weighted
+    isWeighted: PropTypes.bool,
+    // Name / Question of the poll
+    name: PropTypes.string,
+    // A hashmap of key = option and
+    // value = amount of votes
+    options: PropTypes.object,
+  }),
+};
+
+// Default Props if none are provided
+const defaultProps = {
+  poll: {
+    _id: '123',
+    isWeighted: false,
+    name: 'Default Poll',
+    options: {
+      'Option 1': 0,
+      'Option 2': 0,
+    },
+  },
+};
 
 // Component for a page that displays a poll
 class ViewPoll extends Component {
@@ -24,6 +58,7 @@ class ViewPoll extends Component {
     this.handleHandleChange = this.handleHandleChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
     this.handleVoteSubmit = this.handleVoteSubmit.bind(this);
+    this.handleSliderChange = this.handleSliderChange.bind(this);
 
     this.state = {
       handle: '',
@@ -43,6 +78,17 @@ class ViewPoll extends Component {
     } else {
       newSelectedOptions[option] = 1;
     }
+
+    this.setState({
+      selectedOptions: newSelectedOptions,
+    });
+  }
+
+  // Handler for the slider of weighted options
+  handleSliderChange(value, option) {
+    const newSelectedOptions = { ...this.state.selectedOptions };
+
+    newSelectedOptions[option] = value;
 
     this.setState({
       selectedOptions: newSelectedOptions,
@@ -83,7 +129,7 @@ class ViewPoll extends Component {
       });
 
       // Create a new Poll object to be saved
-      let updatedPoll = { ...this.props.poll };
+      const updatedPoll = { ...this.props.poll };
 
       // Remove the _id key to pass validation
       delete updatedPoll._id;
@@ -136,7 +182,20 @@ class ViewPoll extends Component {
   }
 
   renderOptions() {
-    const { options } = this.props.poll;
+    const { options, isWeighted } = this.props.poll;
+
+    if (isWeighted) {
+      return Object.keys(options).map(option => (
+        <Col key={option} md={7} xs={8} className="margin-bottom">
+          <p>{option}</p>
+          <SliderWithTooltip
+            onAfterChange={val => this.handleSliderChange(val, option)}
+            min={0}
+            max={10}
+          />
+        </Col>
+      ));
+    }
 
     return Object.keys(options).map(option => (
       <Col key={option} md={7} xs={8}>
@@ -195,18 +254,10 @@ class ViewPoll extends Component {
   }
 }
 
-// A property containing the polls is required of the data.
-// This object is defined here by the style guidelines
-// PollPage.propTypes = {
-//   poll: PropTypes.arrayOf(PropTypes.shape({
-//     name: PropTypes.string.isOptional,
-//     data: PropTypes.shape({
-//       name: PropTypes.string.isRequired,
-//     }).isRequired,
-//   })).isRequired,
-// };
+ViewPoll.propTypes = propTypes;
+ViewPoll.defaultProps = defaultProps;
 
-// The real MVP; creates the PollPage container using the routing params
+// The real MVP; creates the ViewPoll container using the routing params
 // (the unique url)
 export default createContainer(({ params }) => {
   Meteor.subscribe('polls'); // get the poll database
