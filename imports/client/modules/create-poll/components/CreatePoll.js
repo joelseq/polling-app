19f6585e7d42836cc906fc91c2b95cb82d163561
simsinght <simsinght@gmail.com>
2017-03-11 16:36:06 -0800

    fuck these merge issues

--- imports/client/modules/create-poll/components/CreatePoll.js
+++ imports/client/modules/create-poll/components/CreatePoll.js
@@ -158,12 +158,8 @@ class CreatePoll extends Component {
   // Handler for creating a poll
   handlePollCreate() {
     // Destructuring the state object
-<<<<<<< HEAD
-    const { name, isWeighted, options, isPrivate, password, editPass, isTimed, expiresAt} =
-=======
     const { name, isWeighted, options, isPrivate, 
-      password, editPass, voterEditable } =
->>>>>>> development
+      password, editPass, voterEditable, editPass, isTimed, expiresAt } =
       this.state;
     if (name === '') {
       this.setState({ pollNameError: 'No poll name provided!' });
@@ -201,12 +197,9 @@ class CreatePoll extends Component {
         isPrivate,
         password,
         editPassword: editPass,
-<<<<<<< HEAD
         isTimed,
         expiresAt,
-=======
         isVoterEditable: voterEditable,
->>>>>>> development
       }, (err, result) => {
         if (err || !result) {
           // TODO: add proper error handling
@@ -324,82 +317,68 @@ class CreatePoll extends Component {
             />
             <FormControl.Feedback />
           </FormGroup>
-<<<<<<< HEAD
           <Row>
             <Col md={4}>
-              <FormGroup controlId={'weighted'}>
-                <ControlLabel>Weighted?</ControlLabel>
-                <p className="CreatePoll__info">
-                  Weighted: Votes for options can have weights ranging from 1 to 10 (both inclusive)
-                </p>
-                <p className="CreatePoll__info">
-                  Unweighted: Votes for options are weighted equally
-                </p>
-                {/* These radio buttons are now 'controlled' as well
-                  * Further reading: same link as above
-=======
-          <FormGroup controlId={'weighted'}>
-            <ControlLabel>Weighted?</ControlLabel>
-            <p className="CreatePoll__info">
-              Weighted: Votes for options can have weights ranging from 1 to 10 (both inclusive)
-            </p>
-            <p className="CreatePoll__info">
-              Unweighted: Votes for options are weighted equally
-            </p>
-            {/* These radio buttons are now 'controlled' as well
-              * Further reading: same link as above
-              */}
-            <Radio
-              onChange={() => this.handleWeightedChange(true)}
-              checked={this.state.isWeighted}
-            >
-              Yes
-            </Radio>
-            <Radio
-              onChange={() => this.handleWeightedChange(false)}
-              checked={!this.state.isWeighted}
-            >
-              No
-            </Radio>
-          </FormGroup>
-          <FormGroup controlId={'private'}>
-            <ControlLabel>Private?</ControlLabel>
-            <Radio
-              onChange={() => this.handlePrivateChange(true)}
-              checked={this.state.isPrivate}
-            >
-              Yes
-            </Radio>
-            <Radio
-              onChange={() => this.handlePrivateChange(false)}
-              checked={!this.state.isPrivate}
-            >
-              No
-            </Radio>
-          </FormGroup>
-          <FormGroup controlId={'voterEditable'}>
-            <ControlLabel>Allow Voters to Add and Remove Options?</ControlLabel>
-            <Radio
-              onChange={() => this.handleVoterEditChange(true)}
-              checked={this.state.voterEditable}
-            >
-              Yes
-            </Radio>
-            <Radio
-              onChange={() => this.handleVoterEditChange(false)}
-              checked={!this.state.voterEditable}
-            >
-              No
-            </Radio>
-          </FormGroup>
-          { this.state.isPrivate
-            ?
+            <FormGroup controlId={'weighted'}>
+              <ControlLabel>Weighted?</ControlLabel>
+              <p className="CreatePoll__info">
+                Weighted: Votes for options can have weights ranging from 1 to 10 (both inclusive)
+              </p>
+              <p className="CreatePoll__info">
+                Unweighted: Votes for options are weighted equally
+              </p>
+              {/* These radio buttons are now 'controlled' as well
+                * Further reading: same link as above
+                */}
+              <Radio
+                onChange={() => this.handleWeightedChange(true)}
+                checked={this.state.isWeighted}
+              >
+                Yes
+              </Radio>
+              <Radio
+                onChange={() => this.handleWeightedChange(false)}
+                checked={!this.state.isWeighted}
+              >
+                No
+              </Radio>
+            </FormGroup>
+            <FormGroup controlId={'private'}>
+              <ControlLabel>Private?</ControlLabel>
+              <Radio
+                onChange={() => this.handlePrivateChange(true)}
+                checked={this.state.isPrivate}
+              >
+                Yes
+              </Radio>
+              <Radio
+                onChange={() => this.handlePrivateChange(false)}
+                checked={!this.state.isPrivate}
+              >
+                No
+              </Radio>
+            </FormGroup>
+            <FormGroup controlId={'voterEditable'}>
+              <ControlLabel>Allow Voters to Add and Remove Options?</ControlLabel>
+              <Radio
+                onChange={() => this.handleVoterEditChange(true)}
+                checked={this.state.voterEditable}
+              >
+                Yes
+              </Radio>
+              <Radio
+                onChange={() => this.handleVoterEditChange(false)}
+                checked={!this.state.voterEditable}
+              >
+                No
+              </Radio>
+            </FormGroup>
+              { this.state.isPrivate
+              ?
               <FormGroup controlId={'password'}>
                 <ControlLabel>Password: </ControlLabel>
                 {/* This component is now 'controlled'. Further reading:
-                  * https://facebook.github.io/react/docs/forms.html
->>>>>>> development
-                  */}
+                  * https://facebook.github.io/react/docs/forms.html */}
                 <Radio
                   onChange={() => this.handleWeightedChange(true)}
                   checked={this.state.isWeighted}
@@ -430,8 +409,8 @@ class CreatePoll extends Component {
                   No
                 </Radio>
               </FormGroup>
-              { this.state.isPrivate
-                ?
+                { this.state.isPrivate
+                  ?
                   <FormGroup controlId={'password'}>
                     <ControlLabel>Password: </ControlLabel>
                     {/* This component is now 'controlled'. Further reading:
