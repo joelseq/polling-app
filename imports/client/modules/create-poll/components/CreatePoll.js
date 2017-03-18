84f6d07c400f2319583037ca3927810427f701f3
simsinght <simsinght@gmail.com>
2017-03-18 02:09:01 -0700

    space between 'Weighted' and the (?)

--- imports/client/modules/create-poll/components/CreatePoll.js
+++ imports/client/modules/create-poll/components/CreatePoll.js
@@ -344,7 +344,7 @@ class CreatePoll extends Component {
                 <Row>
                   <FormGroup controlId={'weighted'}>
                     <Col md={6}>
-                      <ControlLabel>Weighted</ControlLabel>
+                      <ControlLabel>Weighted</ControlLabel>{' '}
                       <PopOver />
                     </Col>
                       {/*<p className="CreatePoll__info">
